e0b44c825a61ee850fede7b0c6c168b76e007fc7
FredGit2 <fred.calka@gmail.com>
2023-02-10 15:34:56 +0100

    modification placement footer, mise en place fond gris sur images et modification src images d origine

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -12,7 +12,7 @@ const Collapse = ({ title, content }) => {
         <li>
           <div className="collapse__title" onClick={() => setIsOpen(!isOpen)}>
            <span className="title">{title}</span>
-           <span>{isOpen ? <button className="collapse__open" ><img src={arrow} alt="" /> </button> : <button className="collapse__closed"><img src={arrow} alt="" /></button>}</span>
+           <span>{isOpen ? <button className="collapse__open" ><img src={arrow} alt="flèche" /> </button> : <button className="collapse__closed"><img src={arrow} alt="flèche" /></button>}</span>
         </div>
           {isOpen && <div className="collapse__content">{content}</div>}
         </li>
